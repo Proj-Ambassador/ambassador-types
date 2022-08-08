5969073c79f6f695f9387a81463b629a47534db5
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2022-08-07 21:39:02 -0400

    added isVariableAmount

--- src/PaymentPlan/index.ts
+++ src/PaymentPlan/index.ts
@@ -5,6 +5,7 @@ import { TierDocument } from '../Tier';
 export interface PaymentPlanDocument extends Document {
     name: string;
     isFixedAmount: boolean;
+    isVariableAmount: boolean;
     fixedAmount: number;
     isWebhook: boolean;
     webhookUrl: string;
